be6185cb09b3e51c3239ef0d9d0145c6e04a4e2c
reberan <reberan@gmail.com>
2020-01-14 18:57:20 +0100

    .

--- src/store/modules/counter/actions.js
+++ src/store/modules/counter/actions.js
@@ -1,4 +1,16 @@
 export default {
+  /**
+   * @link https://vuex.vuejs.org/guide/actions.html
+   * Action handlers receive a context object which exposes the same set of
+   * methods/properties on the store instance, so you can call
+   * - context.commit to commit a mutation, or access the state and getters via
+   * - context.state and
+   * - context.getters.
+   * We can even call other actions with
+   * - context.dispatch.
+   * Inside module actions, context.state will expose the local state, and root state will be exposed as
+   * - context.rootState
+   */
   increment: (context, payload) => context.commit("increment", payload),
 
   decrement: ({ commit }, payload) => commit("decrement", payload),
@@ -13,7 +25,7 @@ export default {
   asyncDecrement: ({ commit }, payload) => {
     const { by, wait } = payload;
     setTimeout(() => {
-      commit("increment", by);
+      commit("decrement", by);
     }, wait);
   }
 };
