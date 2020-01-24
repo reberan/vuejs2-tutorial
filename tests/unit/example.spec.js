8042938adcef08b24922284abea55db8c2ce0309
reberan <reberan@gmail.com>
2020-01-24 05:07:43 +0100

    Test fixed

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,9 @@
 import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import Exercise1 from "@/components/exercises/exercise1/";
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    });
-    expect(wrapper.text()).toMatch(msg);
+describe("Exercise1.vue", () => {
+  it("Renders correctly", () => {
+    const wrapper = shallowMount(Exercise1);
+    expect(wrapper.text()).not.toBeNull();
   });
 });
